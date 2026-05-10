62a5e0d3f5d41a43e38de5de786bd39a9b8ac2d9
Step Fk <stephanie020207@gmail.com>
2026-05-10 22:21:48 +0700

    feat: add decoration on about section

--- components/sections/AboutSection.tsx
+++ components/sections/AboutSection.tsx
@@ -17,6 +17,42 @@ export default function AboutSection() {
       />
 
       <div className="absolute inset-0 z-[2] bg-white/50" />
+      <div className="absolute left-[-180] top-0 -translate-y-1/2 z-[7] w-619">
+        <NextImage
+          src="/images/Pattern.svg"
+          alt="dekorasi"
+          width={619}
+          height={606}
+          wrapperClassName="absolute"
+        />
+      </div>
+      <div className="absolute left-[-400] top-17 -translate-y-1/2 z-[7] w-870">
+        <NextImage
+          src="/images/Vector1.svg"
+          alt="dekorasi"
+          width={870}
+          height={198}
+          wrapperClassName="absolute"
+        />
+      </div>
+      <div className="absolute left-300 top-60 -translate-y-1/2 z-[6] w-397">
+        <NextImage
+          src="/images/Exclude.svg"
+          alt="dekorasi"
+          width={397}
+          height={392}
+          wrapperClassName="absolute"
+        />
+      </div>
+      <div className="absolute left-250 top-30 -translate-y-1/2 z-[7] w-619">
+        <NextImage
+          src="/images/Pattern.svg"
+          alt="dekorasi"
+          width={619}
+          height={606}
+          wrapperClassName="absolute"
+        />
+      </div>
       <TypographyContainer
         as="div"
         maxWidth="xl"
